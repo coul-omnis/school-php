ee2cfe3993ca5e497f196d285f6c2be9057f9297
Coul <koufirst.work@gmail.com>
2021-05-13 03:56:13 +0900

    Create command.sql

---
+++ Mysql基礎/command.sql
@@ -0,0 +1,45 @@
+
+--ログインしたら文字化け対策をしておく（xamppの時だけ）
+-- xamppは一旦cp932に設定する。コマンドプロンプトがcp932の為、utf8でも文字化けする。回避策はあるが、そこの説明までは今回は時間が掛かるのでしません。と学生に伝える。
+
+-- やり方
+-- １，xamppのコントロールパネルのmysqlのconfigのmy.iniを開く
+-- ２，character　を検索する
+-- ３，character-set-serverのところを全部cp932にする
+-- ４，default-character-set=cp932　も行う
+-- ※先頭に#マークがついていたら、それはコメント扱い
+
+
+
+-- ログイン
+mysql -u ユーザー名 -p パスワード
+--xamppのデフォルト
+mysql - u root 
+
+-- 上記だけで入れる
+
+
+
+
+-- データベースを作る
+create database test;
+
+-- DB上の文字コードの確認
+show variables like "%chara%";
+-- %の意味はワイルドカードで、前後に何か文字が入っている場合等
+-- likeは曖昧検索。ワイルドカードとセットで使う事が多い
+
+-- テーブル作成
+create table samplebox (id int auto_increment not null primary key,name varchar(100));
+
+insert into samplebox (name) values("名前ちゃん");
+insert into samplebox (name) values("佐藤");
+insert into samplebox (name) values("極道");
+insert into samplebox (name) values("東西南北");
+
+-- auto_incrementは自動的に番号を与えていくオプション
+-- not null　はnullを許可しないように設定する
+-- primary key　はこのテーブルの主キーとなる。（あとでリレーショナルを行う際に設定しておく事になる。キーを設定すると他のテーブルとの連携がしやすくなる）
+
+-- 全カラム抽出
+select * from samplebox;
